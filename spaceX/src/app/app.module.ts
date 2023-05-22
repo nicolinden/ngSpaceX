b8207b4f1a2e3f30f9596dc1169e79b19c0d9a0e
Nico van der Linden <nicolinden@gmail.com>
2023-05-22 10:46:11 +0200

    step2-content

--- spaceX/src/app/app.module.ts
+++ spaceX/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LaunchListComponent]
 })
 export class AppModule { }
